b40b7a98d0cc93552e36978c5700c5c7819e0f60
Carolina <offcarolina@hotmail.com>
2017-11-14 01:55:20 +0000

    Introdução, instalação e MVC

---
+++ ci/application/controllers/Base.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base extends CI_Controller {
+
+	function __construct(){
+	    parente::__construct();
+	}
+	
+	public function index()	{
+		//método padrão do controller
+	}
+}
---
+++ ci/application/controllers/Exemplo1.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Exemplo1 extends CI_Controller {
+
+	function __construct(){
+	    parent::__construct();
+	}
+	
+	public function index()	{
+		echo 'Executado método index do controller';
+	}
+	
+	public function login()	{
+		echo 'Executado método login do controller e passando o parâmetro';
+		echo $this->uri->segment(3);
+	}
+	
+}
